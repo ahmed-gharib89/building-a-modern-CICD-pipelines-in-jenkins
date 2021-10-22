210a849b02271b34fcef72ab0517bbbfe9624eae
Ahmed Gharib <a.gharib89@yahoo.com>
2021-10-22 13:47:37 +0200

    Update test_container with the right port 8000

--- scripts/test_container.sh
+++ scripts/test_container.sh
@@ -6,13 +6,14 @@ until [ "$started" = true ] || [[ ( "$count" == 3 ) ]]; do
   count=$((count+1))
   echo "[$STAGE_NAME] Starting container [Attempt: $count]"
 
-  testStart=$(curl --write-out '%{http_code}' --silent --output /dev/null http://localhost:8080)
+  testStart=$(curl --write-out '%{http_code}' --silent --output /dev/null http://localhost:8000)
   echo "$testStart"
 
   if [[ ( "$testStart" == 200 ) ]]; then
     started=true
     echo "Success"
-    else
+  else
+    started=false
     sleep 1
   fi
 done
